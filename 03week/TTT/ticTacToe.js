d02242c8c8d1c7c946a65a6959fd339701c7b298
alaynapuck28 <you@example.com>
2019-04-21 21:24:30 -0500

    ttt gui functions

--- 03week/TTT/ticTacToe.js
+++ 03week/TTT/ticTacToe.js
@@ -1,137 +1,95 @@
 'use strict';
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+
+let playerTurn = 'X';
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
 ];
-
-let playerTurn = 'X';
-
-function printBoard() {
-  console.log('   0  1  2');
-  console.log('0 ' + board[0].join(' | '));
-  console.log('  ---------');
-  console.log('1 ' + board[1].join(' | '));
-  console.log('  ---------');
-  console.log('2 ' + board[2].join(' | '));
-}
-
 function horizontalWin() {
-  
   // Possible horizontal wins
-  let winByH = board[0][0];
-  if ((board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn) || (board[1][0] == playerTurn && board[1][1] == playerTurn && board[1][2] == playerTurn) || (board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn)) {
-    return true;
-  }
+  if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
 
+  } else if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  } else if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
+  } else if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  } else if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') {
+  document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
+  } else if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  }
+  return true;
 }
 
 function verticalWin() {
-// Possible vertical wins
-
-let winByV = board[0][0];
-if ((board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn) || (board[0][1] == playerTurn && board[1][1] == playerTurn && board[2][1] == playerTurn) || (board[0][2] == playerTurn && board[1][2] == playerTurn && board[2][2] == playerTurn)) {
+  // Possible vertical wins
+  if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
+  } else if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  } else if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
+  } else if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  } else if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
+  } else if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  }
   return true;
 }
-}
+
 
 function diagonalWin() {
-// Possible diagnol wins
-let winByD = board[0][0];
-if ((board[0][0] == playerTurn && board[1][1] == playerTurn && board[2][2] == playerTurn) || (board[2][0] == playerTurn && board[1][1] == playerTurn && board[0][2] == playerTurn)) {
+  // Possible diagonal wins
+  if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
+  } else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  } else if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>X Wins</div>";
+  } else if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
+    document.getElementById("winMessage").innerHTML = "<div id='innerMessage'>O Wins</div>";
+  }
   return true;
 }
 
-}
-
-// Check for all winning options 
 function checkForWin() {
-  if (verticalWin()) {
-    return true;
-  } else if (horizontalWin()) {
-    return true;
-  } else if (diagonalWin()) {
-      return true;
-  }
-
+  horizontalWin();
+  verticalWin();
+  diagonalWin();
+  return true;
 }
 
 function ticTacToe(row, column) {
-//update board based on row and column
-if (playerTurn=='X') {
-board[row][column]= playerTurn;
-playerTurn='O';
-}else if (playerTurn=='O'){
-board[row][column]= playerTurn;
-playerTurn='X'
+  if (board[row][column] === ' ') {
+    switch (playerTurn) {
+    case 'X': 
+      board[row][column] = playerTurn;
+      let letterX = document.getElementById('b'+row+column);
+      letterX.innerHTML = 'X';
+      checkForWin();
+      playerTurn = 'O';
+      break;
+    case 'O':
+      board[row][column] = playerTurn;
+      let letterO = document.getElementById('b'+row+column);
+      letterO.innerHTML = 'O';
+      checkForWin();
+      playerTurn = 'X';
+      break;
+    }
+  } else {
+    ticTacToe();
+  }
 }
-}
-
-
-function getPrompt() {
-printBoard();
-console.log("It's Player " + playerTurn + "'s turn.");
-rl.question('row: ', (row) => {
-  rl.question('column: ', (column) => {
-    ticTacToe(row, column);
-    getPrompt();
-  });
-});
-
-}
-
-function getPrompt() {
-  printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
-  rl.question('row: ', (row) => {
-    rl.question('column: ', (column) => {
-      ticTacToe(row, column);
-      getPrompt();
-    });
-  });
-
-}
-
-
-
-// Tests
-
-if (typeof describe === 'function') {
-
-  describe('#ticTacToe()', () => {
-    it('should place mark on the board', () => {
-      ticTacToe(1, 1);
-      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-    });
-    it('should alternate between players', () => {
-      ticTacToe(0, 0);
-      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-    });
-    it('should check for vertical wins', () => {
-      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-      assert.equal(verticalWin(), true);
-    });
-    it('should check for horizontal wins', () => {
-      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
-      assert.equal(horizontalWin(), true);
-    });
-    it('should check for diagonal wins', () => {
-      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
-      assert.equal(diagonalWin(), true);
-    });
-    it('should detect a win', () => {
-      assert.equal(checkForWin(), true);
-    });
-  });
-} else {
-
-  getPrompt();
-
+//reset game via page reload
+//update later
+function reloadFunction() {
+  location.reload();
 }

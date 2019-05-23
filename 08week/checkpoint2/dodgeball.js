58824b0ab6ee29a0af03686dc94e01ba0a2c6486
alaynapuck28 <you@example.com>
2019-05-23 17:29:44 -0500

    dodgeball functional

--- 08week/checkpoint2/dodgeball.js
+++ 08week/checkpoint2/dodgeball.js
@@ -132,26 +132,150 @@ const listPeopleChoices = () => {
     const li = document.createElement('li');
     const button= document.createElement('button');
     button.innerHTML="Make new player";
+    //turn person into player
     button.addEventListener('click', function(){ makePlayer(person.id);
       listOfPeople.removeChild(li)});
       li.appendChild(button);
       li.appendChild(document.createTextNode("Player: "+ person.name + " " + person.skillSet))
       listOfPeople.append(li);
+       
     }) 
 };
 
-const makePlayer = (id) => {
-  const listOfPlayers = document.getElementById('players');
-  const findPlayerById = arrOfPeople.find(function(entry){
-    return entry.id==id;
-  });
-  const playerIndex = arrOfPeople.indexOf(findPlayerById);
-  const newPlayer= new Player (findPlayerById.id, findPlayerById.name, findPlayerById.age, findPlayerById.skillSet, findPlayerById.placeBorn, true, true, true, true, 3);
-  listOfPlayers.push(newPlayer);
-  arrOfPeople.splice(playerIndex,1);
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newPlayer.name + "-"));
-  listOfPlayers.append(li);
-  //create blue and red buttons 
+//function attached to join blue team button
+// function joinBlueTeam(player){
+//     let blueList= document.getElementById('blue');
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode("Player: "+ player.name + " " + player.skillSet))
+//     blueList.append(li)
+//   }
 
-}
+  //function attached to join red team function
+// function joinRedTeam(player){
+//     redTeam.push(player);
+//     let redList= document.getElementById('red');
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode("Player: "+ player.name + " " + player.skillSet))
+//     redList.append(li);
+//   }
+
+const joinRedTeam = (id) =>{
+  const playersRed = document.getElementById('red');
+  playersRed.style.backgroundColor = 'red';
+  playersRed.style.border = 'solid black 2px';
+  playersRed.style.color = 'yellow';
+  const selectRed = listOfPlayers.find(function(pick){
+  return pick.id == id;
+  });
+  const redIndex = listOfPlayers.indexOf(selectRed);
+  
+  const newRedPlayer = new RedTeammate (selectRed.id, selectRed.name, selectRed.age, selectRed.skillSet, selectRed.placeBorn, true, true, true, true, 4, 'Mister Jelly Bear', 'Red');
+  redTeam.push(newRedPlayer);
+  listOfPlayers.splice(redIndex, 1);
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newRedPlayer.name + " - " + "Mascot = " + newRedPlayer.mascot + " and Team Color is " + newRedPlayer.teamColor));
+  playersRed.append(li);
+  }
+
+  const joinBlueTeam = (id) =>{
+    const playersBlue = document.getElementById('blue');
+    playersBlue.style.backgroundColor = 'blue';
+    playersBlue.style.color = 'white';
+    playersBlue.style.border = 'solid black 2px';
+    const selectBlue = listOfPlayers.find(function(pick){ 
+    return pick.id == id; 
+    });
+    const blueIndex = listOfPlayers.indexOf(selectBlue);
+    
+    const newBluePlayer = new BlueTeammate (selectBlue.id, selectBlue.name, selectBlue.age, selectBlue.skillSet, selectBlue.placeBorn, true, true, true, true, 4, 'Dragon', 'Blue');
+    blueTeam.push(newBluePlayer);
+    listOfPlayers.splice(blueIndex, 1); 
+    
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(newBluePlayer.name + " - " + "Mascot = " + newBluePlayer.mascot + " and Team Color is " + newBluePlayer.teamColor));
+    playersBlue.append(li);
+    }
+
+
+  //change from people to player
+  //////////////////////////////////////////////////////////////////
+// const makePlayer = (id) => {
+//   const newListOfPeople = document.getElementById('players');
+//   const findPlayerById = arrOfPeople.find(function(entry){
+//     return entry.id==id;
+//   });
+//   const playerIndex = arrOfPeople.indexOf(findPlayerById);
+//   const newPlayer= new Player(findPlayerById.id, findPlayerById.name, findPlayerById.age, findPlayerById.skillSet, findPlayerById.placeBorn, true, true, true, true, 3);
+  
+//   //push players to listOfPlayers array
+//   listOfPlayers.push(newPlayer)
+//   arrOfPeople.splice(playerIndex, 1);
+//   console.log(listOfPlayers)
+//   console.log(arrOfPeople)
+  
+//   listOfPlayers.map(player =>{
+
+//     //create red and blue button for every player
+//     const li = document.createElement('li');
+//     const redButton= document.createElement('button');
+//     const blueButton= document.createElement('button');
+
+//     //button text
+//     blueButton.innerHTML="Join blue team";
+//     redButton.innerHTML="Join red team";
+
+//     redButton.addEventListener('click', function() {joinRedTeam(player.id)});
+//       // newListOfPeople.removeChild(li);
+//       li.appendChild(redButton);
+//       newListOfPeople.append(li);
+//       console.log(redTeam)
+
+//     blueButton.addEventListener('click', function() {joinBlueTeam(player.id)});
+//       // newListOfPeople.removeChild(li);
+//       li.appendChild(blueButton);
+//       li.appendChild(document.createTextNode("Player: "+ player.name + " " + player.skillSet))
+//       newListOfPeople.append(li); 
+//   })} 
+  //////////////////////////////////////////////////////////////////////////
+
+  const makePlayer = (id) => {
+    const players = document.getElementById('players');
+    const findPlayer = arrOfPeople.find(function(entry){ 
+    return entry.id == id; 
+    });
+    const playerIndex = arrOfPeople.indexOf(findPlayer);
+    //this is the generic instance of the Player Class
+    const newPlayer = new Player (findPlayer.id, findPlayer.name, findPlayer.age, findPlayer.skillSet, findPlayer.placeBorn, true, true, true, true, 4);
+    listOfPlayers.push(newPlayer);
+    arrOfPeople.splice(playerIndex, 1);
+    //this adds people to list of players DOM
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(newPlayer.name + " - "));
+    
+    const button_blue = document.createElement("button");
+    button_blue.style.backgroundColor='Blue';
+    button_blue.style.color = 'white';
+    button_blue.style.border = 'solid 1px black';
+    button_blue.innerHTML = "Blue Team";
+    button_blue.addEventListener('click', function() {joinBlueTeam(newPlayer.id);
+    players.removeChild(li);})
+    li.appendChild(button_blue);
+    const button_red = document.createElement("button");
+    button_red.style.backgroundColor='Red';
+    button_red.style.color = 'yellow';
+    button_red.style.border = 'solid 1px black';
+    button_red.innerHTML = "Red Team";
+    button_red.addEventListener('click', function() {joinRedTeam(newPlayer.id);
+    players.removeChild(li);})
+    li.appendChild(button_red);
+    
+    players.append(li);
+    }
+
+
+  // arrOfPeople.splice(playerIndex,1);
+
+
+  // const li = document.createElement('li');
+  // li.appendChild(document.createTextNode(newPlayer.name + "-"));
+  // listOfPlayers.append(li);
